96acf8e9cd4b8fd2fa36915091e01ee20bd6b968
Amós Silas <silastj@hotmail.com>
2018-10-16 00:54:09 -0300

    Index.php placeholder e form width

--- config.php
+++ config.php
@@ -4,7 +4,7 @@
 try {
     global $pdo;
    // $pdo = new PDO('mysql:host=localhost;dbname=projeto_mmn;charset=utf8', 'root', '');
-    $pdo = new PDO('mysql:host=67.23.245.159;dbname=ctdntmed_projeto_mmn;charset=utf8', 'ctdntmed_silas', 'ams9339');
+    $pdo = new PDO('mysql:host=;dbname=ctdntmed_projeto_mmn;charset=utf8', '', '');
     $pdo->setAttribute(PDO::ERRMODE_EXCEPTION, PDO::ATTR_ERRMODE);
 
 } catch(PDOException $e) {
